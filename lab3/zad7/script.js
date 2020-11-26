71b90f3539bb50d844b9ac44288f403ca180ec8d
Kamil Delekta <dkamil17@wp.pl>
2020-11-26 12:23:24 +0100

    lab3/zad7 updated

--- lab3/zad7/script.js
+++ lab3/zad7/script.js
@@ -6,6 +6,7 @@ function validateForm(){
     // Check if inputs are filled!
     for(var input of inputs){
         if(input.value === ""){
+            // Wypisywanie dokładnego labela do danego pola
             for(var label of labels){
                 if(input.id === label.htmlFor){
                     alert("Uzupełnij [" + label.textContent +  "] !")
@@ -22,26 +23,68 @@ function validateForm(){
         alert("Uzupełnij [ Płeć: ] !")
     }
 
-    canRetire();
+    onlyLetter();
     phoneValidation();
+    canRetire();
 
 
 }
 
+function onlyLetter(){
+    var texts = document.querySelectorAll("input[type=text]");
+    var labels = document.getElementsByTagName('label');
+
+    var pattern = /^[A-Za-z]+$/
+    for(var text of texts){
+        if(text.value != ""){
+            if(!text.value.match(pattern)){
+                // Wypisywanie dokładnego labela do danego pola
+                for(var label of labels){
+                    if(text.id === label.htmlFor){
+                        alert("W polu [" +  label.textContent +  "] dozwolone sa jedynie litery!")
+                    }
+                }
+                
+            }
+        }
+        
+    }
+}
+
+function phoneValidation(){
+    var phoneNumber = document.getElementById("phone");
+
+    var pattern = /^[0-9]{9}$/
+    if(phoneNumber.value != ""){      
+        if(!phoneNumber.value.match(pattern)){
+            alert("Niepoprawny format numeru telefonu. Wymagamu format 123456789 (cyfry)")
+        }
+    }
+};
+
+// Sprawdzam czy osoba składająca wniosek osiągnie wiek emerytalny w dniu podanym jako [Data przejscia na emeryture]
 function canRetire(){
 
-    // ISO format of Date YYYY-MM-DD
+    // ISO format of Date YYYY-MM-DD, on Chrome there is no problem with format of input
     var birth = document.getElementById("birth").value;
     var retirementDate = document.getElementById("retirement-date").value;
 
     var men = document.getElementById("men");
     var women = document.getElementById("women");
 
-    if(birth != " " && retirementDate != " " && (men.checked || women.checked)){
+    if(birth != "" && retirementDate != "" && (men.checked || women.checked)){
         var d1 = new Date(birth);
         var d2 = new Date(retirementDate);
 
+
         var age = d2.getTime() - d1.getTime();
+
+        leapYears = howManyLeapYears(d1.getFullYear(), d2.getFullYear())
+        console.log("Number of leap years: " + leapYears)
+
+        // Dodaje ilosc dnie przestepnych
+        age += leapYears * 1000 * 3600 * 24
+
 
         var age = age / (1000 * 3600 * 24 * 365)
         console.log(age);
@@ -66,9 +109,20 @@ function canRetire(){
     }
 }
 
-// To Do
-function phoneValidation(){
 
-};
+function isYearLeap(year)
+{
+  return !((year % 4) && (year % 100) || !(year % 400));
+}
+
+function howManyLeapYears(year1, year2){
+    counter = 0;
+    for( var i = year1; i <= year2; i++){
+        if(isYearLeap(i)){
+            counter++;
+        }
+    }
+    return counter;
+}
 
 
